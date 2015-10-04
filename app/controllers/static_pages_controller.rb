f7001c31adb86bb06c1057c4fd519354b2a70eda
grnsimplicity <connieh57@gmail.com>
2015-10-04 15:37:42 -0700

    landpage picture correct now

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
       @issues = Issue.all
+      @narratives= Narrative.all
   end
 
   def home
       @issues = Issue.all
+      @narratives= Narrative.all
   end
 
   def test
